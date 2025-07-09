be1fcac399c2daa7acf8201aabd3339afbb391c8
artemmolodoy1487 <myanowitch@yandex.ru>
2025-07-09 18:24:50 +0300

    chore: delete validations from models, now only in dto

--- src/main/java/by/starovoytov/userservice/model/CardInfo.java
+++ src/main/java/by/starovoytov/userservice/model/CardInfo.java
@@ -2,24 +2,15 @@ package by.starovoytov.userservice.model;
 
 import by.starovoytov.userservice.converter.YearMonthAttributeConverter;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import java.time.YearMonth;
 
-import org.hibernate.validator.constraints.CreditCardNumber;
-
-
 @Data
 @Entity
-@Table(name = "card_info", indexes = {
-    @Index(name = "idx_cardinfo_user_id", columnList = "user_id")
-})
+@Table(name = "card_info", indexes = { @Index(name = "idx_cardinfo_user_id", columnList = "user_id") })
 public class CardInfo {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,17 +21,12 @@ public class CardInfo {
     @ToString.Exclude
     private User user;
 
-    @NotBlank(message = "Card number cannot be blank")
-    @CreditCardNumber(message = "Card number is invalid")
     @Column(nullable = false)
     private String number;
 
-    @NotBlank(message = "Card holder name cannot be blank")
     @Column(nullable = false)
     private String holder;
-
-    @NotNull(message = "Expiration date must be provided")
-    @Future(message = "Card expiration date must be in the future")
+    
     @Convert(converter = YearMonthAttributeConverter.class)
     @Column(name = "expiration_date", nullable = false)
     private YearMonth expirationDate;
--- src/main/java/by/starovoytov/userservice/model/User.java
+++ src/main/java/by/starovoytov/userservice/model/User.java
@@ -1,48 +1,34 @@
 package by.starovoytov.userservice.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "users", indexes = {
-    @Index(name = "idx_user_email", columnList = "email", unique = true)
-})
+@Table(name = "users", indexes = { @Index(name = "idx_user_email", columnList = "email", unique = true) })
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name cannot be blank")
     @Column(nullable = false)
     private String name;
 
-    @NotBlank(message = "Surname cannot be blank")
     @Column(nullable = false)
     private String surname;
 
-    @NotNull(message = "Birth date must be provided")
-    @Past(message = "Birth date must be in the past")
     @Column(nullable = false)
     private LocalDate birthDate;
 
-    @NotBlank(message = "Email cannot be blank")
-    @Email(message = "Email should be valid")
     @Column(nullable = false, unique = true)
     private String email;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private Set<CardInfo> cards = new HashSet<>();
