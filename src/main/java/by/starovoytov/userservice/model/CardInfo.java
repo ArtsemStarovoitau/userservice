5b882981c2e011505b0dfeac49d905cdb4a43938
artemmolodoy1487 <myanowitch@yandex.ru>
2025-07-09 15:32:08 +0300

    feat: Add fk user cardinfo

--- src/main/java/by/starovoytov/userservice/model/CardInfo.java
+++ src/main/java/by/starovoytov/userservice/model/CardInfo.java
@@ -4,20 +4,28 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @Entity
 @Table(name = "card_info")
-class CarfInfo {
+public class CardInfo { 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
-    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @EqualsAndHashCode.Exclude 
+    @ToString.Exclude 
+    private User user; 
+
     private String number;
     private String holder;
     private String expirationDate;
--- src/main/java/by/starovoytov/userservice/model/User.java
+++ src/main/java/by/starovoytov/userservice/model/User.java
@@ -1,18 +1,24 @@
 package by.starovoytov.userservice.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @Entity
 @Table(name = "users")
-class User {
+public class User { 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +29,9 @@ class User {
 
     private LocalDate birthDate;
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @EqualsAndHashCode.Exclude // Exclude from Lombok's equals and hashCode to prevent StackOverflowError
+    @ToString.Exclude // Exclude from Lombok's toString to prevent infinite loop
+    private Set<CardInfo> cards = new HashSet<>(); // Initialize to avoid NullPointerException
 }
