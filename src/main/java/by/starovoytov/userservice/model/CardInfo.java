7b84914200ee798b0ca960f970e4488ddea30ed5
artemmolodoy1487 <myanowitch@yandex.ru>
2025-07-09 16:51:12 +0300

    feat: Add hibernate validation for valid credit card number

--- src/main/java/by/starovoytov/userservice/model/CardInfo.java
+++ src/main/java/by/starovoytov/userservice/model/CardInfo.java
@@ -1,32 +1,47 @@
 package by.starovoytov.userservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import by.starovoytov.userservice.converter.YearMonthAttributeConverter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import java.time.YearMonth;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 
 @Data
 @Entity
-@Table(name = "card_info")
-public class CardInfo { 
+@Table(name = "card_info", indexes = {
+    @Index(name = "idx_cardinfo_user_id", columnList = "user_id")
+})
+public class CardInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    @EqualsAndHashCode.Exclude 
-    @ToString.Exclude 
-    private User user; 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private User user;
 
+    @NotBlank(message = "Card number cannot be blank")
+    @CreditCardNumber(message = "Card number is invalid")
+    @Column(nullable = false)
     private String number;
+
+    @NotBlank(message = "Card holder name cannot be blank")
+    @Column(nullable = false)
     private String holder;
-    private String expirationDate;
+
+    @NotNull(message = "Expiration date must be provided")
+    @Future(message = "Card expiration date must be in the future")
+    @Convert(converter = YearMonthAttributeConverter.class)
+    @Column(name = "expiration_date", nullable = false)
+    private YearMonth expirationDate;
 }
--- src/main/java/by/starovoytov/userservice/model/User.java
+++ src/main/java/by/starovoytov/userservice/model/User.java
@@ -1,37 +1,49 @@
 package by.starovoytov.userservice.model;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @Entity
-@Table(name = "users")
-public class User { 
+@Table(name = "users", indexes = {
+    @Index(name = "idx_user_email", columnList = "email", unique = true)
+})
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name cannot be blank")
+    @Column(nullable = false)
     private String name;
+
+    @NotBlank(message = "Surname cannot be blank")
+    @Column(nullable = false)
     private String surname;
 
+    @NotNull(message = "Birth date must be provided")
+    @Past(message = "Birth date must be in the past")
+    @Column(nullable = false)
     private LocalDate birthDate;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    @Column(nullable = false, unique = true)
     private String email;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    @EqualsAndHashCode.Exclude // Exclude from Lombok's equals and hashCode to prevent StackOverflowError
-    @ToString.Exclude // Exclude from Lombok's toString to prevent infinite loop
-    private Set<CardInfo> cards = new HashSet<>(); // Initialize to avoid NullPointerException
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Set<CardInfo> cards = new HashSet<>();
 }
